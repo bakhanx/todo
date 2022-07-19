fc5559341d74b12bbc910d9f83f59814a250561a
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2022-07-19 20:46:00 +0900

    useform

--- src/Components/ToDoList.tsx
+++ src/Components/ToDoList.tsx
@@ -1,21 +1,71 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface IForm {
+  id: string;
+  password: string;
+  email: string;
+}
 
 const ToDoList = () => {
-  const [toDoValue, setToDoValue] = useState("");
-  const onChange = (event:React.FormEvent<HTMLInputElement>)=>{
-    const {currentTarget :{value}} = event;
-    setToDoValue(value);
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
 
-  }
-  const onSubmit = (event:React.FormEvent<HTMLFormElement>)=>{
-    event.preventDefault();
-    console.log(toDoValue);
+  const onValid = (data: any) => {
+    console.log(data);
+  };
 
-  }
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} placeholder="write a to do"></input>
+      <form
+        onSubmit={handleSubmit(onValid)}
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <input
+          {...register("id", {
+            required: "id is required!",
+            minLength: {
+              value: 5,
+              message: "your password is short",
+            },
+          })}
+          placeholder="ID"
+        />
+        <span>{errors?.id?.message}</span>
+        <input
+          {...register("password", {
+            required: "password is required",
+            minLength: {
+              value: 5,
+              message: "your password is short",
+            },
+          })}
+          placeholder="Password"
+        />
+        <span>{errors?.password?.message}</span>
+        <input
+          {...register("email", {
+            required: "Email is required",
+            minLength: {
+              value: 5,
+              message: "your password is short",
+            },
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver\.com$/,
+              message: "Only naver.com",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message}</span>
         <button>Add</button>
       </form>
     </div>
